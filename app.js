08d08d13b54ca5b5c35bc11ef404adceeba5de25
ConradWilliam <92802571+ConradWilliam@users.noreply.github.com>
2023-02-04 10:11:55 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = (<ul>
+  <li>Menu</li>
+  <li>Contacts</li>
+  <li>People</li>
+  <li>Services</li>
+</ul>)
+
+ReactDOM.render(myList, document.getElementById('app'));
