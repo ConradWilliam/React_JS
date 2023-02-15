6c441a186caa8890752befe9b80607906527a079
ConradWilliam <92802571+ConradWilliam@users.noreply.github.com>
2023-02-15 21:37:30 +0300

    Add files via upload

---
+++ Components And Advanced JSX/render.js
@@ -0,0 +1,7 @@
+class MyComponentClass extends React.Component
+{ // everything in between these curly-braces is instructions for how to build components
+ 
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
---
+++ Variable Name/app.js
@@ -0,0 +1,25 @@
+
+
+
+const owl = {
+    title: 'Excellent Owl',
+    src: 'https://content.codecademy.com/courses/React/react_photo-owl.jpg'
+  };
+  
+  // Component class starts here:
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  
+  class Owl extends React.Component{
+    render(){
+      return (<div>
+        <h1>{owl.title}</h1>
+          <img
+          src = {owl.src}
+           alt = {owl.title}/>
+      </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(<Owl />, document.getElementById('app'))
